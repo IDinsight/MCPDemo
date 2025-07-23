382c2ab37b10828ab0c75d2e44353e1ff73f10c5
tonyzhao6 <>
2025-07-23 17:09:59 -0400

    CCs.

--- backend/src/mcp_demo/gunicorn_hooks.py
+++ backend/src/mcp_demo/gunicorn_hooks.py
@@ -1,8 +1,5 @@
 """This module contains the gunicorn hooks configuration for the application."""
 
-# Standard Library
-import os
-
 # Third Party Library
 from gunicorn.arbiter import Arbiter
 from prometheus_client import multiprocess
@@ -11,8 +8,6 @@ from uvicorn_worker import UvicornWorker
 
 class Worker(UvicornWorker):
     """Custom worker class to allow `root_path` to be passed to Uvicorn."""
-
-    CONFIG_KWARGS = {"root_path": os.getenv("API_BACKEND_ROOT", "")}
 
 
 def child_exit(server: Arbiter, worker: Worker) -> None:  # pylint: disable=W0613
